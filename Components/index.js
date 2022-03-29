0f59fb1527b45b8a945ca3f09cd0afa73fda9209
ccarver80 <ckc_80@outlook.com>
2022-03-28 20:34:25 -0600

    got bonus buttons working and functioning

--- Components/index.js
+++ Components/index.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Text, TextInput, View, Button, ImageBackground } from "react-native";
+import {
+  Text,
+  TextInput,
+  ScrollView,
+  View,
+  Button,
+  ImageBackground,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import tw from "twrnc";
 
@@ -7,67 +14,80 @@ export default function Home() {
   const [bank, setBank] = useState(1000000);
   const [name, setName] = useState();
   const [oilPrice, setOilPrice] = useState(50);
+  const [oilProduction, setOilProduction] = useState(0);
   const [hour, setHour] = useState(0);
   const [day, setDay] = useState(1);
-  const [gameStart, setGameStart] = useState(true)
-    
-  function getRandomOilPrice() {
-    if(oilPrice > 20 && oilPrice < 150 ){
-        let rdmNum = (Math.random() * (1.2 - .80) + .80) 
-    console.log(rdmNum.toFixed(2))
-    return rdmNum.toFixed(2); 
-    }
+  const [gameStart, setGameStart] = useState(true);
+  const [multiplyer, setMultiplier] = useState(5);
+  const [clockSpeed, setClockSpeed] = useState(1000);
 
-    if(oilPrice < 20) {
-      let rdmNum = (Math.random() * (1.5 - .95) + 0.95)
-      console.log(rdmNum.toFixed(2))
+  function getRandomOilProduction(cost) {
+    let rdmNum = Math.random() * multiplyer;
+    setBank(bank - cost);
+    return rdmNum;
+  }
+
+  function getRandomOilPrice() {
+    if (oilPrice > 20 && oilPrice < 150) {
+      let rdmNum = Math.random() * (1.2 - 0.8) + 0.8;
       return rdmNum.toFixed(2);
     }
-  
-    
+
+    if (oilPrice < 20) {
+      let rdmNum = Math.random() * (1.5 - 0.95) + 0.95;
+
+      return rdmNum.toFixed(2);
+    }
+
+    if (oilPrice > 150) {
+      let rdmNum = Math.random() * (1.05 - 0.5) + 0.5;
+
+      return rdmNum.toFixed(2);
+    }
   }
 
   useEffect(() => {
-    if (gameStart) { 
+    if (gameStart) {
       // Time clock to add each second as if it was 1 hour, and add 1 day after 24 seconds elapse
       setTimeout(() => {
         if (hour < 23) {
           setHour(hour + 1);
-          setOilPrice(oilPrice * getRandomOilPrice())
+          console.log(multiplyer)
         } else {
           setHour(0);
           setDay(day + 1);
-          setBank(bank + 1000)
-          
-          ;
-        }
-      }, 1000);
-    }
-  }, [ hour, day]);
+          setOilPrice(oilPrice * getRandomOilPrice());
 
- 
+          setBank(bank + oilPrice * oilProduction);
+        }
+      }, clockSpeed);
+    }
+  }, [day, hour]);
 
   return (
     <>
       <View style={tw.style("bg-blue-100 p-5 flex ")}>
-        <Text style={tw.style("text-black text-7 mt-10 text-center")}>
-          Day: {day} Hour:{hour}
-        </Text>
         <Text
           style={tw.style(
-            "text-center mt-2 text-10 bg-blue-500 text-black rounded-xl"
+            "text-center mt-10 text-10 bg-blue-500 text-black rounded-xl"
           )}
         >
           Oilfield Game
         </Text>
-        <Text style={tw.style("text-center mt-2 text-10 text-black")}>
-          Bank: {`$${bank}`}
+        <Text style={tw.style("text-black text-7 mt-2 text-center")}>
+          Day: {day} Hour:{hour}
         </Text>
-        <Text style={tw.style("text-center mt-2 text-10 text-black")}>
-          Field Name: {name}
+
+        <Text style={tw.style("font-bold text-center mt-2 text-10 text-black")}>
+          Bank: {`$${bank.toFixed(2)}`}
         </Text>
+
         <Text style={tw.style("text-center mt-2 text-8 text-black")}>
           Oil Price: {oilPrice.toFixed(2)}/per bbl
+        </Text>
+
+        <Text style={tw.style("bg-red-200 text-center mt-2 text-8 text-black")}>
+          Oil Prod: {oilProduction.toFixed(2)}bbls / per Day
         </Text>
 
         <StatusBar style="auto" />
@@ -77,9 +97,50 @@ export default function Home() {
         style={tw.style("h-[100%]")}
         source={require("../assets/oilbackground.jpg")}
       >
-        <View>
-          <Button  title="Click to start" />
-        </View>
+        <ScrollView>
+          <View>
+            <Button
+              onPress={() => {
+                if (bank >= 100000) {
+                  setOilProduction(
+                    oilProduction + getRandomOilProduction(100000)
+                  );
+                }
+              }}
+              title="Drill well (Cost: $100,000)"
+            />
+
+            <Button
+              onPress={() => {
+                if (bank >= 50000) {
+                  setOilProduction(oilProduction * 1.1);
+                  setBank(bank - 50000);
+                }
+              }}
+              title="Acidize Wells $50,000 (increase Oil Prod by 10%)"
+            />
+
+            <Button
+              onPress={() => {
+                if (bank >= 10000) {
+                  setOilPrice(oilPrice * 1.2);
+                  setBank(bank - 10000);
+                }
+              }}
+              title="Influence Market $10,000 (increase Oil price by 20%)"
+            />
+
+            <Button
+              onPress={() => {
+                if (bank >= 50000) {
+                  setMultiplier(multiplyer * 1.2);
+                  setBank(bank - 50000);
+                }
+              }}
+              title="Drill Better Wells $50,000 (Base prod increase by 20% on each new well)"
+            />
+          </View>
+        </ScrollView>
       </ImageBackground>
     </>
   );
